65aed4b50cba966b43ffad5be662c7e4e1b8e15e
Andreev Danila <mail@neandreev.ru>
2021-04-06 17:24:06 +0500

    Make "getName" function to return player name

--- src/cli.js
+++ src/cli.js
@@ -1,9 +1,11 @@
 import readlineSync from 'readline-sync';
 
-const welcome = () => {
+const getName = () => {
   console.log('Welcome to the Brain Games!');
   const name = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${name}`);
+
+  return name;
 };
 
-export default welcome;
+export default getName;
