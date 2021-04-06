1906be0e4f57e99ed05da50ebf80508a69a45f9d
Andreev Danila <mail@neandreev.ru>
2021-04-06 22:46:12 +0500

    Add export before every const

--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,6 @@
 import readlineSync from 'readline-sync';
 
-const getName = () => {
+export const getName = () => {
   console.log('Welcome to the Brain Games!');
   const name = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${name}`);
@@ -8,20 +8,16 @@ const getName = () => {
   return name;
 };
 
-const getAnswer = () => readlineSync.question('Your answer: ');
-const showQuestion = (question) => console.log(`Question: ${question}`);
+export const getAnswer = () => readlineSync.question('Your answer: ');
+export const showQuestion = (question) => console.log(`Question: ${question}`);
 
-const evenRules = () => console.log('Answer "yes" if the number is even, otherwise answer "no".');
-const calcRules = () => console.log('What is the result of the expression?');
+export const evenRules = () => console.log('Answer "yes" if the number is even, otherwise answer "no".');
+export const calcRules = () => console.log('What is the result of the expression?');
 
-const wrong = (name, answer, correctAnswer) => {
+export const wrong = (name, answer, correctAnswer) => {
   console.log(`"${answer}" is wrong answer ;(. Correct answer was "${correctAnswer}".`);
   console.log(`Let's try again, ${name}!`);
 };
 
-const correct = () => console.log('Correct!');
-const congrats = (name) => console.log(`Congratulations, ${name}!`);
-
-export {
-  getName, getAnswer, showQuestion, evenRules, calcRules, wrong, correct, congrats,
-};
+export const correct = () => console.log('Correct!');
+export const congrats = (name) => console.log(`Congratulations, ${name}!`);
