96178bec98779e628f0bc0e5a5cf36d7e96ccb2d
Andreev Danila <mail@neandreev.ru>
2021-04-06 22:25:43 +0500

    Add new game "brain-calc"

---
+++ src/brainCalc.js
@@ -0,0 +1,28 @@
+import _ from 'lodash';
+import { calcRules } from './cli.js';
+import playGame from './engine.js';
+
+const getRandomExpression = () => {
+  const expressions = [
+    ['+', ((a, b) => a + b)],
+    ['-', ((a, b) => a - b)],
+    ['*', ((a, b) => a * b)],
+  ];
+
+  return expressions[_.random(2)];
+};
+
+const getQuestionAndAnswer = () => {
+  const firstRandomInt = _.random(20);
+  const secondRandomInt = _.random(20);
+  const randomExpression = getRandomExpression();
+
+  const question = `${firstRandomInt} ${randomExpression[0]} ${secondRandomInt}`;
+  const correctAnswer = randomExpression[1](firstRandomInt, secondRandomInt);
+
+  return [question, correctAnswer.toString()];
+};
+
+export default () => {
+  playGame(calcRules, getQuestionAndAnswer);
+};
